4849e4c8ca3a257746dc9c2b3db4161617892eba
zachmann00 <zach_mann@hotmail.com>
2021-03-01 09:59:52 -0500

    complete assignment

---
+++ decisionMakingExercise.js
@@ -0,0 +1,32 @@
+// 1a
+let randomNum = Math.random();
+randomNum *= 5;
+randomNum = Math.ceil(randomNum);
+console.log(randomNum);
+
+// 1b
+if (randomNum >= 4) {
+    console.log("Greater than or equal to 4");
+  } else if (randomNum == 2, 3)  {
+    console.log("Equal to 2 or 3");
+  } else (randomNum = 1) 
+    console.log("Equal to 1");
+  
+// 1c
+if (randomNum !== 3){
+    console.log("Not equal to 3");
+}
+
+// 1d
+if (randomNum !== 3 && 5) {
+    console.log("NOT equal to 3 AND not equal to 5");
+}
+
+//  1e
+if (randomNum === 2 || 4) {
+    console.log("Equal to 2 OR equal to 4");
+}
+
+// BONUS
+// 2 
+if (randomNum  )
